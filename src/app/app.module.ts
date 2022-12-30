e804e5a02ea6df52ceb21f551d5ba045873a1b60
Kapibarra <nikita.nov2016@yandex.ru>
2022-12-30 15:03:39 +0300

    login page components nd services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { GalleryComponent } from './homepage/shared/components/gallery/gallery.component';
+import { HeaderComponent } from './homepage/shared/components/header/header.component';
+import { FormComponent } from './homepage/shared/components/form/form.component';
+import { LoginComponent } from './homepage/shared/components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    GalleryComponent
+    GalleryComponent,
+    HeaderComponent,
+    FormComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
