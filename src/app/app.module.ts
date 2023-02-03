dbc93009d4abb3b0fcaaad879c5cc5b2e1f8f026
kapibarra2315 <nikita.nov2016@yandex.ru>
2023-02-03 13:36:22 +0300

    grid gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { FormComponent } from './homepage/shared/components/form/form.component';
 import { GalleryComponent } from './homepage/shared/components/gallery/gallery.component';
+import { HeaderComponent } from './homepage/shared/components/header/header.component';
+import { LoginComponent } from './homepage/shared/components/login/login.component';
 
 @NgModule({
   declarations: [
